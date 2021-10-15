e7cd3f9fe3320498df9cfa3d46ab54434ceb0ec2
voorhs <ilya_alekseev_2016@list.ru>
2021-10-15 23:38:43 +0300

    теперь точно работает

--- Clusterization-algorithm/clust.py
+++ Clusterization-algorithm/clust.py
@@ -2,70 +2,115 @@ import numpy as np
 from sklearn.datasets import make_blobs
 
 class KMeans:
-    def __init__(self, n_clusters=2):
-        self.n_clusters = n_clusters
-    
-    def initilize_centroids(self, eur):
-        if eur.lower() == "sample":
-            self.indeces = np.random.choice(np.arange(self.X.shape[0]), self.n_clusters, replace=False)
-            self.centroids = np.copy(self.X[self.indeces])
-            self.set_labels()
-        elif eur.lower() == "random":
-            self.indeces = None
-            min_x, min_y = np.min(self.X, axis=0)
-            max_x, max_y = np.max(self.X, axis=0)
-            y = np.random.rand(self.n_clusters) * (max_y - min_y) + min_y
-            x = np.random.rand(self.n_clusters) * (max_x - min_x) + min_x
-            self.centroids = np.vstack([x,y]).T
-            self.assignment()
+    def __init__(self, X, n_clusters):
+        """
+        X is array of points (x, y)
+        """
 
-    def fit(self, X, eur="sample"):
         self.X = X
-        self.initilize_centroids(eur)
-        self.labels = self.labels.astype(int)
-        i = 0
-        while (np.linalg.norm(self.centroids - self.update()) > 0.1):
-            self.assignment()
-            i += 1
-        return self
+        self.n_clusters = n_clusters
+        self.n_points = X.shape[0]
+    
+    def init_random(self):
+        """
+        initializes array of centroids using 'random' strategy:
+        random 2dim points from the area of X
+        """
 
-    def near_ind(self, ind):     
-        if ind in self.indeces:
-            return ind
-        
-        res = self.indeces[0]
-        for i in self.indeces:
-            if np.linalg.norm(self.X[ind] - self.X[i]) < np.linalg.norm(self.X[ind] - self.X[res]):
-                res = i
+        min_x, min_y = np.min(self.X, axis=0)
+        max_x, max_y = np.max(self.X, axis=0)
+
+        y_coords = np.random.rand(self.n_clusters) * (max_y - min_y) + min_y
+        x_coords = np.random.rand(self.n_clusters) * (max_x - min_x) + min_x
+
+        self.centroids = np.vstack([x_coords,y_coords]).T
+
+    def init_sample(self):
+        """
+        initializes array of centroids using 'sample' strategy:
+        random simple sample from X
+        """
+
+        self.indeces = np.random.choice(np.arange(self.X.shape[0]), self.n_clusters, replace=False)
+        self.centroids = np.copy(self.X[self.indeces])
+    
+    def compute_distance_matrix(self, centr_ind):   
+        """
+        returns matrix 'centr_ind' times 'n_clusters' where
+        [i][j] is distance between point X[i] and centroids[j],
+        i.e. rho( X[i], centroid[j] )
+        """     
+        res = np.zeros((self.n_points, centr_ind))
+        for i in range(0, self.n_points):
+            for j in range(0, centr_ind):
+                res[i][j] = np.linalg.norm(self.centroids[j] - self.X[i])
         return res
+
+    def init_distant(self):
+        """
+        initializes array of centroids using 'distant' strategy:
+        1st centroid is random point of X, 2nd is the farthest point of X from 1st centroid,
+        3rd is the farthest point of X from 1st ans 2nd centroids etc.
+        """
+        self.indeces = np.zeros(self.n_clusters, dtype=int)
+        self.centroids = np.zeros((self.n_clusters, 2))
+
+        t = np.random.randint(0, self.n_points, size=1)
+        self.indeces[0] = t
+        self.centroids[0] = self.X[t]
+
+        for i in range(1, self.n_clusters):
+            t = np.argmax(self.compute_distance_matrix(i).mean(axis=1))
+            self.indeces[i] = t
+            self.centroids[i] = self.X[t]
+
+    def init_centr(self, heur):
+        """
+        initializes centroids with given heuristic
+        """
+        dct = {
+            "sample": self.init_sample,
+            "random": self.init_random,
+            "distant": self.init_distant,
+        }
+        dct[heur]()
     
-    def set_labels(self):
-        self.labels = np.zeros(self.X.shape[0])
-        dct = zip(list(self.indeces), list(np.arange(0, self.n_clusters)))
-        dct = dict(dct)        
-        for i in range(self.X.shape[0]):
-            self.labels[i] = dct[self.near_ind(i)]
-    
-    def near(self, x):
+    def near_center(self, point):
+        """
+        returns the index of the nearest centroids from given point
+        """
         res = 0
-        for i in range(self.centroids.shape[0]):
-            if np.linalg.norm(x - self.centroids[i]) < np.linalg.norm(x - self.centroids[res]):
+        for i in range(self.n_clusters):
+            if np.linalg.norm(point - self.centroids[i]) < np.linalg.norm(point - self.centroids[res]):
                 res = i
         return res
 
-    def assignment(self):
-        self.labels = np.zeros(self.X.shape[0])
-        for i in range(self.X.shape[0]):
-            self.labels[i] = self.near(self.X[i])
+    def set_labels(self):
+        """
+        sets labels of membership in the cluster
+        """
+        self.labels = np.zeros(self.n_points)
+        for i in range(self.n_points):
+            self.labels[i] = self.near_center(self.X[i])  
 
-    def update(self):
-        self.centroids *= 0        
-        counts = np.zeros(self.n_clusters)
-        for i in range(self.X.shape[0]):            
-            self.centroids[self.labels[i]] += self.X[i]
-            counts[self.labels[i]] += 1
-        for i in range(self.centroids.shape[0]):
-            self.centroids[i] /= counts[i]
+    def update_centers(self):
+        """
+        compute new centroids for every cluster
+        """
+        self.centroids = np.zeros((self.n_clusters, 2))
+        for i in range(self.n_clusters):
+            self.centroids[i] = self.X[self.labels == i].mean(axis=0)
         return self.centroids
+    
+    def fit(self, heur="random", prec=0.001):
+        """
+        main function that launches centroids and labels initialization
+        and executes iterative k-means algorithm
+        """
+        self.init_centr(heur)
+        self.set_labels()
 
-X, y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
+        while(np.linalg.norm(self.centroids - self.update_centers()) > prec):
+            self.set_labels()
+        
+        return self
